061624584badee0909333bd06eda8b0f23f518d1
Evan Kapantais <evankapantais@gmail.com>
2019-07-25 17:06:19 +0200

    wip, basic rig, starter functions, style card wip

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 let library = [];
 
 class Book {
-  constructor(name, author, pages, read) {
+  constructor(name, author, date, read) {
     this.name = name,
     this.author = author,
-    this.pages = pages,
+    this.date = date,
     this.read = read 
   }
 }
 
 Book.prototype.info = function() { 
-  return this.name + " by " + this.author + ", " + this.pages + " pages, " + this.read + ".";}
-
-const book = new Book("Brave New World", "Aldous Huxley", 230, "read");
-console.log(book.info());
-
-function addBookToLibrary(book) {
-  
+  return this.name + " by " + this.author + ", " + this.date + ", " + this.read + ".";
 }
+  
+function addBookToLibrary(book) {
+  library.push(book);
+}
+
+function renderLibrary() {
+  const libraryDiv = document.querySelector(".library");
+  libraryDiv.innerHTML = library;
+}
+
+const book1 = new Book("Brave New World", "Aldous Huxley", 1930, "read");
+const book2 = new Book("Talking To My Daughter", "Yanis Varoufakis", 2016, "not read");
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+console.log(library);
+
+// renderLibrary();
