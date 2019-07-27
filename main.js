e33e048700b9fbf3350450eb1d3d3c968262df9a
evan-kapantais <evankapantais@gmail.com>
2019-07-27 20:29:55 +0200

    get inputs, add book to array, display the card in the correct column

--- main.js
+++ main.js
@@ -1,33 +1,68 @@
 let library = [];
 
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const yearInput = document.querySelector("#year");
+const categoryInput = document.querySelector("#category");
+const readInput = document.querySelector("#read");
+
+const btnAddBook = document.querySelector(".btn--add");
+btnAddBook.addEventListener("click", getBookInfo);
+
+
 class Book {
-  constructor(name, author, date, read) {
-    this.name = name,
+  constructor(title, author, year, category, read) {
+    this.title = title,
     this.author = author,
-    this.date = date,
-    this.read = read 
+    this.year = year,
+    this.category = category,
+    this.read = read
   }
 }
 
 Book.prototype.info = function() { 
-  return this.name + " by " + this.author + ", " + this.date + ", " + this.read + ".";
+  return this.title, this.author, this.year, this.category, this.read;
+}
+
+function getBookInfo() {
+  const book = new Book(titleInput.value, authorInput.value, 
+  yearInput.value, categoryInput.value, readInput.checked);
+
+  addBookToLibrary(book);
+  renderBookCard(book);
+  
+  console.log(book.info());
 }
   
 function addBookToLibrary(book) {
   library.push(book);
+  console.log(library);
 }
 
-function renderLibrary() {
-  const libraryDiv = document.querySelector(".library");
-  libraryDiv.innerHTML = library;
+function renderBookCard(book) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book__card");
+  bookCard.innerHTML = `<p class="book__title">${book.title}</p>
+                        <p class="book__author">${book.author}</p>
+                        <div class="book__footer">
+                          <p class="book__year">${book.year}</p>
+                          <p class="book__category">${book.category}</p>
+                        </div>`;
+  if (book.read == true) {
+    const readColumn = document.querySelector(".column__read");
+    readColumn.appendChild(bookCard);
+  } else {
+    const notReadColumn = document.querySelector(".column__not-read");
+    notReadColumn.appendChild(bookCard);
+  }
 }
 
-const book1 = new Book("Brave New World", "Aldous Huxley", 1930, "read");
-const book2 = new Book("Talking To My Daughter", "Yanis Varoufakis", 2016, "not read");
+function fillInputs() {
+  titleInput.value = "God Is Not Great";
+  authorInput.value = "Christopher Hitchens";
+  yearInput.value = "2005";
+  categoryInput.value = "Non-Fiction";
+  readInput.checked = true;
+}
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-
-console.log(library);
-
-// renderLibrary();
+// fillInputs();
